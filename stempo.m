b0019245e7ef5364bf189828c08fe079207d8e94
Leonardo Viada <leo@revodigital.it>
2022-10-06 14:21:20 +0200

    mnc2223_es1/3: checks delta

--- stempo.m
+++ stempo.m
@@ -2,7 +2,11 @@ v=input('inserire v:');
 h=input('inserire h:');
 g=input('inserire g:');
 
-t1 = (v-sqrt(power(v, 2) - 2*h*g))/g;
-t2 = (v+sqrt(power(v, 2) - 2*h*g))/g;
-
-fprintf('t1=%f, t2=%f\n', t1, t2);
+delta = power(v, 2) - 2*h*g;
+if(delta >= 0)
+    t1 = (v-sqrt(power(v, 2) - 2*h*g))/g;
+    t2 = (v+sqrt(power(v, 2) - 2*h*g))/g;
+    fprintf('t1=%f, t2=%f\n', t1, t2);
+else 
+    fprintf('Soluzioni non reali');
+end
